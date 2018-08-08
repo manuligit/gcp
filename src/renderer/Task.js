e30a35a3582b37557d8b6df66f378144c24ef6f4
manuligit <riina.vaisanen@gmail.com>
2018-08-08 17:26:56 +0300

    Added form for creating tasks and simple saving to local storage

--- src/renderer/Task.js
+++ src/renderer/Task.js
@@ -6,14 +6,17 @@ class Task extends React.Component {
     this.state = {
       name: "",
       done: false,
-      repeat: 0
+      repeat: 0,
+      desc: ""
     }
   }
 
   render() {
     return(
-      <div className="task">
-        {this.props.task}
+      <div className="task" key={this.props.task.task}>
+        <div> {this.props.task.task} </div>
+        <div> {this.props.task.desc}</div>
+        <div>{this.props.task.type}</div>
       </div>
     )
   }
--- src/renderer/TaskList.js
+++ src/renderer/TaskList.js
@@ -5,16 +5,51 @@ class TaskList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ['asdf','bnm', 'cvb'],
-      done: []
+      list: [{task:'asdf', desc:'bnm', type:'Once', done: false}],
+      done: [],
+      type: 'Once'
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.saveToLocalStorage = this.saveToLocalStorage.bind(this);
   }
 
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({type: event.target.value});
+  }
 
+  addTask (event) {
+    event.preventDefault();
+    // //console.log(event.target.task.value)
+    // console.log( event.target.task.value)
+    // console.log(event.target.desc.value)
+    // console.log(this.state.type)
+    let item = {
+      task: event.target.task.value,
+      desc: event.target.desc.value,
+      type: this.state.type,
+      done: false
+    }
 
+    let list = this.state.list.concat(item)
 
-  addTask() {
+    this.setState({ list: list })
+    console.log(list)
 
+    this.saveToLocalStorage(list);
+  }
+
+  saveToLocalStorage(item) {
+    localStorage.setItem('getTasks', JSON.stringify(item));
+  }
+
+  componentDidMount(){
+    let tasks = JSON.parse(localStorage.getItem('getTasks'))
+    console.log(tasks)
+    if (tasks != null) {
+      this.setState({list: tasks})
+    }
   }
 
   render() {
@@ -24,14 +59,47 @@ class TaskList extends React.Component {
       done: false
     }
 
+    let form = (
+      <div className="form">
+        <h3> Add a new task </h3>
+        <form onSubmit={this.addTask}>
+          <div>
+            <label htmlFor="task">
+              Name
+            </label>
+            <input id="task" type="text" name="task" required/>
+          </div>
+          <div>
+            <label htmlFor="task">
+              Description
+            </label>
+            <input id="desc" type="text" name="desc" required/>
+          </div>
+          <div>
+            <label htmlFor="type">
+              Type
+            </label>
+            <select value={this.state.type} onChange={this.handleChange} required>
+              <option value="Once">Once</option> 
+              <option value="Daily">Daily</option> 
+              <option value="Weekly">Weekly</option> 
+              <option value="Monthly" >Monthly</option> 
+            </select>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    )
+
     return(
       <div>
         {this.state.list.map(p => 
-          <div key={p}>
-            <Task task={p} />
+          <div key={p.task}>
+            <Task task={p}/>
             <div className="divider"></div>
           </div>
         )}
+        {form}
       </div>
     )
   }
